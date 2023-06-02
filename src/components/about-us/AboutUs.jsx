5d2eddf127143785bbbb89b594b1549db706cef5
huytruong99th <huytruong99th@outlook.com>
2023-06-02 09:30:47 +0700

    ok

--- src/components/about-us/AboutUs.jsx
+++ src/components/about-us/AboutUs.jsx
@@ -11,7 +11,16 @@ function AboutUs() {
             <article className="about-us-article">
                     <p>BlockACE, founded in 2018, was initially branded as “BlockAsia Vietnam” which provides one-stop solution for international blockchain companies to expand to Vietnam & South East Asia markets including legal & business consultancy, marketing & PR activities. Recent clients include Node Capital, ChainUp, Jinse, Genesis Capital, MXC, WBF…</p>
             </article>
-        </div>
+            <div className="brand-previous-logo">
+              <figure>
+                <img src="/image/BlockACE-prev.png" width="180"></img>
+              </figure>
+              <figure>
+                <img src="/image/BIT.png" width="180"></img>
+              </figure>
+
+            </div>
+          </div>
           <CardSlide />
         </div>
     </section>
