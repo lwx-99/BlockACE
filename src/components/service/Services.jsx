3fb2536e715dda32e515cdf2fdc431f1313575c1
huytruong99th <huytruong99th@outlook.com>
2023-06-02 10:04:23 +0700

    ok

--- src/components/service/Services.jsx
+++ src/components/service/Services.jsx
@@ -4,7 +4,22 @@ function Services() {
   return (
     <>
         <section className="service-section">
-          <h1 className="section-title">About Us</h1>
+          <h1 className="section-title">Our Services</h1>
+
+          <div className="service-card-container">
+            <div className='service-card market-sector'>
+              <h2>Market Sector</h2>
+
+            </div>
+            <div className='service-card business-sector'>
+              <h2>B2B Sector</h2>
+
+            </div>
+            <div className='service-card community-sector'>
+              <h2>Community Sector</h2>
+
+            </div>
+          </div>
         </section>
     </>
   )
