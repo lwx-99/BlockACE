e41b640a9268d7e97bfda10767e7b1428bc01fcd
huytruong99th <huytruong99th@outlook.com>
2023-06-03 12:27:37 +0700

    ok

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,10 +1,91 @@
 import React from 'react'
 
+const project01 = [
+  {
+    logo: "/image/project-logo/algo.svg",
+    project_name: "Algorand"
+  },
+  {
+    logo: "/image/project-logo/busd.svg",
+    project_name: "BUSD"
+  },
+  {
+    logo: "/image/project-logo/chainlink.svg",
+    project_name: "Chainlink"
+  },
+  {
+    logo: "/image/project-logo/chia.svg",
+    project_name: "Chia"
+  },
+  {
+    logo: "/image/project-logo/ether.svg",
+    project_name: "Ethereum"
+  },
+  {
+    logo: "/image/project-logo/fantom.svg",
+    project_name: "Fantom"
+  },
+  {
+    logo: "/image/project-logo/matic.svg",
+    project_name: "Polygon"
+  },
+  {
+    logo: "/image/project-logo/stacks.svg",
+    project_name: "Stacks"
+  },
+  {
+    logo: "/image/project-logo/stellar.svg",
+    project_name: "Stellar"
+  }
+]
+
+const project02 = [
+  {
+    logo: "/image/project-logo/matic.svg",
+    project_name: "Polygon"
+  },
+  {
+    logo: "/image/project-logo/stacks.svg",
+    project_name: "Stacks"
+  },
+  {
+    logo: "/image/project-logo/stellar.svg",
+    project_name: "Stellar"
+  },
+]
+
+const project03 = [
+  {},
+]
+
+
+
 function Projects() {
   return (
     <div className="projects-section">
         <div>
-            <h1 className="projects-section-title">BlockACE's Projects</h1>
+            <h1 className="projects-section-title">Projects</h1>
+        </div>
+        <div className="projects-container">
+
+          <div className="projects-container-line-01">
+            <div className="slide-01">
+              {project01.map((item) => (
+                <div className="project-item">
+                    <img src={item.logo} className="project-item-icon"/>
+                    <p>{item.project_name}</p>
+                </div>
+              ))}
+            </div>
+            <div className="slide-02">
+              {project01.map((item) => (
+                <div className="project-item">
+                    <img src={item.logo} className="project-item-icon"/>
+                    <p>{item.project_name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
     </div>
   )
