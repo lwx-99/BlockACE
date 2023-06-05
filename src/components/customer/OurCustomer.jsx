8d10f14711ea786b22e1fbb8064b872c6b7917a6
huytruong99th <huytruong99th@outlook.com>
2023-06-05 10:36:38 +0700

    ok ok

--- src/components/customer/OurCustomer.jsx
+++ src/components/customer/OurCustomer.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
 
+const CustomerList = [
+  {
+    logo: "/image/customer/Acent.png",
+    customerName: "Acent"
+  },
+  {
+    logo: "/image/customer/Aluna.png",
+    customerName: "Aluna"
+  },
+  {
+    logo: "/image/customer/Bithumb.png",
+    customerName: "Bithumb"
+  },
+  {
+    logo: "/image/customer/Contentos.png",
+    customerName: "Contentos"
+  },
+  {
+    logo: "/image/customer/CrossFi.png",
+    customerName: "CrossFi"
+  },
+  {
+    logo: "/image/customer/dAppstore.png",
+    customerName: "dAppstore"
+  },
+  {
+    logo: "/image/customer/Evrynet.png",
+    customerName: "Evrynet"
+  },
+  {
+    logo: "/image/customer/Velo.svg",
+    customerName: "Velo"
+  },
+  {
+    logo: "/image/customer/XRUN.png",
+    customerName: "XRUN"
+  },
+  {
+    logo: "/image/customer/YourEarthDAO.svg",
+    customerName: "Your Earth DAO"
+  }
+]
+
 function OurCustomer() {
   return (
     <div className="customer-section">
+      <h1 className="section-title">Our Customers</h1>
+      <div className="customer-container">
+          { CustomerList.map( (item) => (
+            <figure>
+              <img src={item.logo} width="220"></img>
+            </figure>
+          ))}
+      </div>
       
     </div>
   )
