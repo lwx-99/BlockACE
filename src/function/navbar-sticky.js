42ed3eb17767fdb2567587c3ee63aef56e4e3e19
huytruong99th <huytruong99th@outlook.com>
2023-06-20 13:56:35 +0700

    123

--- src/function/navbar-sticky.js
+++ src/function/navbar-sticky.js
@@ -6,14 +6,22 @@ function navbarSticky() {
     console.log(measureFromTop)
     var startingPoint = navBar.getBoundingClientRect().y;
 
-   if (measureFromTop >= startingPoint) {
-        navBar.style.top = "20px";
-        navBar.style.opacity = "0.8";
-        navBar.style.transform = "translate(-50%, 0) scale(0.8)";
-   } else {
-        navBar.style.top = "calc(2.5vh + 30px)";
-        navBar.style.opacity = "1";
-        navBar.style.transform = "translate(-50%, 0) scale(1)";
+    if (window.innerWidth > 960) {
 
-   }
+     if (measureFromTop >= startingPoint) {
+          navBar.style.top = "20px";
+          navBar.style.opacity = "0.8";
+          navBar.style.transform = "translate(-50%, 0) scale(0.8)";
+     } else {
+          navBar.style.top = "calc(2.5vh + 30px)";
+          navBar.style.opacity = "1";
+          navBar.style.transform = "translate(-50%, 0) scale(1)";
+  
+     }
+
+    } else {
+          return
+    }
+
+ 
 }
